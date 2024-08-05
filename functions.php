e87a1009c595ed334ec03af027104f570b8d80ac
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-08-06 00:31:33 +0600

    added files

---
+++ functions.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Essential theme supports
+ * */
+function theme_setup()
+{
+    /** automatic feed link*/
+    add_theme_support('automatic-feed-links');
+
+    /** tag-title **/
+    add_theme_support('title-tag');
+
+    /** post formats */
+    $post_formats = array('aside', 'image', 'gallery', 'video', 'audio', 'link', 'quote', 'status');
+    add_theme_support('post-formats', $post_formats);
+
+    /** post thumbnail **/
+    add_theme_support('post-thumbnails');
+
+    /** HTML5 support **/
+    add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
+
+    /** refresh widgest **/
+    add_theme_support('customize-selective-refresh-widgets');
+
+    /** custom background **/
+    $bg_defaults = array(
+        'default-image'          => '',
+        'default-preset'         => 'default',
+        'default-size'           => 'cover',
+        'default-repeat'         => 'no-repeat',
+        'default-attachment'     => 'scroll',
+    );
+    add_theme_support('custom-background', $bg_defaults);
+
+    /** custom header **/
+    $header_defaults = array(
+        'default-image'          => '',
+        'width'                  => 300,
+        'height'                 => 60,
+        'flex-height'            => true,
+        'flex-width'             => true,
+        'default-text-color'     => '',
+        'header-text'            => true,
+        'uploads'                => true,
+    );
+    add_theme_support('custom-header', $header_defaults);
+
+    /** custom log **/
+    add_theme_support('custom-logo', array(
+        'height'      => 60,
+        'width'       => 400,
+        'flex-height' => true,
+        'flex-width'  => true,
+        'header-text' => array('site-title', 'site-description'),
+    ));
+}
+add_action('after_setup_theme', 'theme_setup');
+
+function add_theme_scripts() {
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+	wp_enqueue_style( 'slider', get_template_directory_uri() . '/css/slider.css', array(), '1.1', 'all' );
+
+	wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), 1.1, true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
--- header.php
+++ header.php
@@ -1,32 +1,106 @@
 <!doctype html>
-<html class="no-js" lang="zxx" >
-   <head>
-      <meta charset="utf-8">
-      <meta http-equiv="x-ua-compatible" content="ie=edge">
-      <title>Harry - Creative Agency & Portfoilo Template</title>
-      <meta name="description" content="">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+<html <?php language_attributes(); ?>>
 
-      <!-- Place favicon.ico in the root directory -->
-      <link rel="shortcut icon" type="image/x-icon" href="assets/img/logo/favicon.png">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Harry - Creative Agency & Portfoilo Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-      <!-- CSS here -->
-      <link rel="stylesheet" href="assets/css/bootstrap.css">
-      <link rel="stylesheet" href="assets/css/meanmenu.css">
-      <link rel="stylesheet" href="assets/css/animate.css">
-      <link rel="stylesheet" href="assets/css/swiper-bundle.css">
-      <link rel="stylesheet" href="assets/css/slick.css">
-      <link rel="stylesheet" href="assets/css/nouislider.css">
-      <link rel="stylesheet" href="assets/css/backtotop.css">
-      <link rel="stylesheet" href="assets/css/magnific-popup.css">
-      <link rel="stylesheet" href="assets/css/nice-select.css">
-      <link rel="stylesheet" href="assets/css/font-awesome-pro.css">
-      <link rel="stylesheet" href="assets/css/elegant-icon.css">
-      <link rel="stylesheet" href="assets/css/hover-reveal.css">
-      <link rel="stylesheet" href="assets/css/spacing.css">
-      <link rel="stylesheet" href="assets/css/main.css">
-   </head>
-   <body>
-      <!--[if lte IE 9]>
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <!--[if lte IE 9]>
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
       <![endif]-->
+
+    <!-- header area start -->
+    <header>
+        <div class="header__area">
+            <div class="header__bottom-9 header__sticky" id="header-sticky">
+                <div class="container">
+                    <div class="mega-menu-wrapper p-relative">
+                        <div class="row align-items-center">
+                            <div class="col-xxl-3 col-xl-3 col-lg-2 col-md-4 col-sm-5 col-8">
+                                <div class="logo">
+                                    <a href="index.html">
+                                        <img src="assets/img/logo/logo-green.svg" alt="logo">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xxl-7 col-xl-7 col-lg-8 d-none d-lg-block">
+                                <div class="main-menu main-menu-9 main-menu-ff-space">
+                                    <nav id="mobile-menu">
+                                        <ul>
+                                            <li class="has-dropdown">
+                                                <a href="index.html">Home</a>
+                                                <ul class="submenu">
+                                                    <li><a href="index.html">Main Demo</a></li>
+                                                    <li><a href="index-shop.html">Shop Demo</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="has-dropdown">
+                                                <a href="portfolio.html">Protfolio</a>
+                                                <ul class="submenu">
+                                                    <li><a href="portfolio.html">Classic Style</a></li>
+                                                    <li><a href="portfolio-details-list.html">List With Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="has-dropdown ">
+                                                <a href="#">Pages</a>
+                                                <ul class="submenu">
+                                                    <li><a href="portfolio.html">Portfoilo</a></li>
+                                                    <li><a href="portfolio-details-list.html">Portfoilo Details</a></li>
+                                                    <li><a href="about-me.html">About Me</a></li>
+                                                    <li><a href="cart.html">Cart</a></li>
+                                                    <li><a href="checkout.html">Checkout</a></li>
+                                                    <li><a href="services.html">Services</a></li>
+                                                    <li><a href="services-details.html">Service Details</a></li>
+                                                    <li><a href="404.html">404 Page</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="has-dropdown">
+                                                <a href="blog.html">Blog</a>
+                                                <ul class="submenu">
+                                                    <li><a href="blog.html">Blog Standard</a></li>
+                                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="has-dropdown">
+                                                <a href="shop.html">Shop</a>
+                                                <ul class="submenu">
+                                                    <li><a href="shop.html">Shop</a></li>
+                                                    <li><a href="product-details.html">Product Details</a></li>
+                                                </ul>
+                                            </li>
+                                            <li>
+                                                <a href="contact.html">Contact</a>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                    <!-- for wp -->
+                                    <div class="header__hamburger ml-50 d-none">
+                                        <button type="button" class="hamburger-btn offcanvas-open-btn">
+                                            <span></span>
+                                            <span></span>
+                                            <span></span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-8 col-sm-7 col-4">
+                                <div class="header__bottom-right-8 d-flex justify-content-end align-items-center">
+                                    <div class="header__hamburger ml-50">
+                                        <button type="button" class="hamburger-btn-8 hamburger-btn-8-black offcanvas-open-btn">Menu</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header area end -->
