a705222b40979deafb647a97c5ebf3551e7b855d
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-08-08 02:28:20 +0600

    update

--- header.php
+++ header.php
@@ -16,91 +16,43 @@
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
       <![endif]-->
 
-    <!-- header area start -->
-    <header>
-        <div class="header__area">
-            <div class="header__bottom-9 header__sticky" id="header-sticky">
-                <div class="container">
-                    <div class="mega-menu-wrapper p-relative">
-                        <div class="row align-items-center">
-                            <div class="col-xxl-3 col-xl-3 col-lg-2 col-md-4 col-sm-5 col-8">
-                                <div class="logo">
-                                    <a href="index.html">
-                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo/logo-green.svg" alt="logo">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-xxl-7 col-xl-7 col-lg-8 d-none d-lg-block">
-                                <div class="main-menu main-menu-9 main-menu-ff-space">
-                                    <nav id="mobile-menu">
-                                        <ul>
-                                            <li class="has-dropdown">
-                                                <a href="index.html">Home</a>
-                                                <ul class="submenu">
-                                                    <li><a href="index.html">Main Demo</a></li>
-                                                    <li><a href="index-shop.html">Shop Demo</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="has-dropdown">
-                                                <a href="portfolio.html">Protfolio</a>
-                                                <ul class="submenu">
-                                                    <li><a href="portfolio.html">Classic Style</a></li>
-                                                    <li><a href="portfolio-details-list.html">List With Details</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="has-dropdown ">
-                                                <a href="#">Pages</a>
-                                                <ul class="submenu">
-                                                    <li><a href="portfolio.html">Portfoilo</a></li>
-                                                    <li><a href="portfolio-details-list.html">Portfoilo Details</a></li>
-                                                    <li><a href="about-me.html">About Me</a></li>
-                                                    <li><a href="cart.html">Cart</a></li>
-                                                    <li><a href="checkout.html">Checkout</a></li>
-                                                    <li><a href="services.html">Services</a></li>
-                                                    <li><a href="services-details.html">Service Details</a></li>
-                                                    <li><a href="404.html">404 Page</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="has-dropdown">
-                                                <a href="blog.html">Blog</a>
-                                                <ul class="submenu">
-                                                    <li><a href="blog.html">Blog Standard</a></li>
-                                                    <li><a href="blog-details.html">Blog Details</a></li>
-                                                </ul>
-                                            </li>
-                                            <li class="has-dropdown">
-                                                <a href="shop.html">Shop</a>
-                                                <ul class="submenu">
-                                                    <li><a href="shop.html">Shop</a></li>
-                                                    <li><a href="product-details.html">Product Details</a></li>
-                                                </ul>
-                                            </li>
-                                            <li>
-                                                <a href="contact.html">Contact</a>
-                                            </li>
-                                        </ul>
-                                    </nav>
-                                    <!-- for wp -->
-                                    <div class="header__hamburger ml-50 d-none">
-                                        <button type="button" class="hamburger-btn offcanvas-open-btn">
-                                            <span></span>
-                                            <span></span>
-                                            <span></span>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-xxl-2 col-xl-2 col-lg-2 col-md-8 col-sm-7 col-4">
-                                <div class="header__bottom-right-8 d-flex justify-content-end align-items-center">
-                                    <div class="header__hamburger ml-50">
-                                        <button type="button" class="hamburger-btn-8 hamburger-btn-8-black offcanvas-open-btn">Menu</button>
-                                    </div>
-                                </div>
-                            </div>
+    <!-- pre loader area start -->
+    <div id="loading">
+        <div id="loading-center">
+            <div id="loading-center-absolute">
+                <!-- loading content here -->
+                <div class="preloader__content text-center">
+                    <div class="preloader__logo">
+                        <img src="assets/img/logo/logo-preloader.svg" alt="">
+                    </div>
+                    <div id="tp-loading-bar" class="preloader__bar">
+                        <div id="tp-loading-line" class="preloader__bar-inner"></div>
+                    </div>
+                    <h3 class="preloader__title">harry</h3>
+                    <div class="preloader__with-text ">
+                        <div class="preloader__with-text-wrapper">
+                            <span data-text-preloader="h" class="preloader__title-2">h</span>
+                            <span data-text-preloader="a" class="preloader__title-2">a</span>
+                            <span data-text-preloader="r" class="preloader__title-2">r</span>
+                            <span data-text-preloader="r" class="preloader__title-2">r</span>
+                            <span data-text-preloader="y" class="preloader__title-2">y</span>
                         </div>
                     </div>
+                    <p class="preloader__loading">Loading</p>
                 </div>
             </div>
         </div>
-    </header>
-    <!-- header area end -->
+    </div>
+    <!-- pre loader area end -->
+
+    <!-- back to top start -->
+    <div class="back-to-top-wrapper">
+        <button id="back_to_top" type="button" class="back-to-top-btn">
+            <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11 6L6 1L1 6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+        </button>
+    </div>
+    <!-- back to top end -->
+
+    <?php pHarry_header(); ?>
